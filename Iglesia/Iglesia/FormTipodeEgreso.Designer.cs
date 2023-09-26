07b1cb2ddacfaef7c4a35804ff5791bd883918f2
mel-git <melisag@live.com.ar>
2023-09-26 00:29:09 -0300

    Agrego formularios de Ingreso y egreso de maría
    Copie el diseño de los formularios de ingreso y egreso. El codigo esta mal, pero cuando maria los arregle copio directamente el codigo nuevo.

---
+++ Iglesia/Iglesia/FormTipodeEgreso.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ControlIngresoEgreso.Formularios
+{
+    partial class FormTipodeEgreso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 225);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(381, 288);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 22);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tipo de Egreso";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Datos";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormTipodeEgreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 567);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "FormTipodeEgreso";
+            this.Text = "FormTipodeEgreso";
+            this.Load += new System.EventHandler(this.FormTipodeEgreso_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Iglesia/Iglesia/FormTipodeIngreso.cs
@@ -0,0 +1,110 @@
+﻿using ControlIngresoEgreso.Clases;
+using ControlIngresoEgreso.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iglesia
+{
+    public partial class FormTipodeIngreso : Form
+    {
+        public FormTipodeIngreso()
+        {
+            InitializeComponent();
+
+        }
+        bool Editar;
+        int IdTipodeIngreso;
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Validacion()) return;
+            if (!Guardar()) return;
+
+            finalizar();
+        }
+        private void finalizar()
+        {
+            ListarGrid();
+            limpiar();
+        }
+
+        private void limpiar()
+        {
+            txtIngreso.Text = "";
+            Editar = false;
+        }
+        private bool Guardar()
+        {
+            TipoIngreso tipoIngreso = new TipodeIngreso
+            {
+                Denominacion = txtIngreso.Text,
+                IdTipodeIngreso = IdtipodeIngreso
+            };
+            return TipoIngreso.Guardar(TipodeIngreso, Editar);
+        }
+          private void finalizar()
+           {
+            ListarGrid();
+            limpiar();
+            }
+
+     private void limpiar()
+    {
+        txtIngreso.Text = "";
+        Editar = false;
+    }
+    
+
+        private bool Validacion() 
+        { 
+            if (string.IsNullOrWhiteSpace(txtIngreso.Text))
+
+            { MessageBox.Show("Ingresar Denominacion"); 
+                return false; 
+            }   
+                
+             return false; 
+            return true; 
+        }
+        private void finalizar()
+        {
+            ListarGrid();
+            Limpiar();
+        }
+
+        private void FormTipodeIngreso_Load(object sender, EventArgs e)
+        {
+            ListarGrid();
+
+        }
+        private void ListarGrid()
+        {
+            dvgDatos.DataSource = TipodeIngreso.Listar();
+            Dbdatos.OcultarIds(dgvDatos);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
+
